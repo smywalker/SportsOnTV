7c02f7c219404ddd8dc4e91a9664523339f0472e
Luke Smy <smywalker22@gmail.com>
2016-12-04 18:42:58 +0000

    Completed animation

--- app/src/main/java/com/smashedcontrollers/sportsontv/splash.java
+++ app/src/main/java/com/smashedcontrollers/sportsontv/splash.java
@@ -1,13 +1,39 @@
 package com.smashedcontrollers.sportsontv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        imageView = (ImageView)findViewById(R.id.logoIV);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_animation);
+        imageView.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
+
 }

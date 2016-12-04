624a5ee382d3162c1a94cfb93c11f844f5b7ef1e
Luke Smy <smywalker22@gmail.com>
2016-12-04 21:25:17 +0000

    added google sign in button

--- app/src/main/java/com/smashedcontrollers/sportsontv/MainActivity.java
+++ app/src/main/java/com/smashedcontrollers/sportsontv/MainActivity.java
@@ -3,8 +3,12 @@ package com.smashedcontrollers.sportsontv;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

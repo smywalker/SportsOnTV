808681d834a48ab3b627830c3c64de66307193a1
Luke Smy <smywalker22@gmail.com>
2016-12-05 17:11:12 +0000

    added menu class

--- app/src/main/java/com/smashedcontrollers/sportsontv/MainActivity.java
+++ app/src/main/java/com/smashedcontrollers/sportsontv/MainActivity.java
@@ -1,48 +1,50 @@
 package com.smashedcontrollers.sportsontv;
-
-import android.support.annotation.NonNull;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.view.ActionMode;
+import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+//import com.firebase.ui.auth.AuthUI;
+//import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    //constants
-    private static int RC_SIGN_IN = 0;
-    private static  String TAG = "MAIN_ACTIVITY";
 
-    //variables
-    private GoogleApiClient mGoogleApiClient;//how we are going to connect to google
-    private FirebaseAuth mAuth;// how to authenticate to firebase
-    private FirebaseAuth.AuthStateListener mAuthListener;//listens for changes in the authentication
-    @Override
+    LoginButton loginButton;
+    CallbackManager callbackManager;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        //see auth status when create an activity
-        mAuth = FirebaseAuth.getInstance();
-        //set up listener to see when we log in and out
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                // check to see if user is there
-                if(user != null){
-                    Log.d("AUTH", "user logged in" + user.getEmail());
-
-                }else{
-                    Log.d("AUTH", "user logged out.");
-                }
+            public void onSuccess(LoginResult loginResult) {
+                Intent i = new Intent(MainActivity.this,)
             }
-        };
-        //used to sign in, what we want to receive back from google
-        //aswell as how we want to send our requests
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail().build();
+
+            @Override
+            public void onCancel() {
+                Toast cancel = Toast.makeText(MainActivity.this ,"Sign in Cancelled", Toast.LENGTH_SHORT);
+                cancel.show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode, data);
     }
 }

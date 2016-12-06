198070a5442444323a08d12f92dc10705c6ee1ff
Luke Smy <smywalker22@gmail.com>
2016-12-06 22:25:47 +0000

    deleted broken code

--- app/src/main/java/com/smashedcontrollers/sportsontv/Menu.java
+++ app/src/main/java/com/smashedcontrollers/sportsontv/Menu.java
@@ -7,34 +7,11 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class Menu extends AppCompatActivity {
-    private static ImageButton ballButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-    }
-    //onclick method to to
-    public void onButtonClick(View view){
-        switch (view.getId()) {
-            case R.id.ballImageButton:
-
-                break;
-            case R.id.basketBallButton:
-                break;
-            case R.id.musicButton:
-                break;
-            case R.id.yubeButton:
-                break;
-            case R.id.pictureButton:
-                Intent ballIntent = new Intent(".Gallery");
-                startActivity(ballIntent);
-                break;
-            case R.id.feedbackButton:
-                break;
-            default:
-                break;
-
-        }
     }
 
 }

c99f31166ae264a4c37d2d49e74cd036cd5d0ec5
Luke Smy <smywalker22@gmail.com>
2016-12-06 01:40:55 +0000

    added sound effect to splash screen

--- app/src/main/java/com/smashedcontrollers/sportsontv/splash.java
+++ app/src/main/java/com/smashedcontrollers/sportsontv/splash.java
@@ -1,18 +1,25 @@
 package com.smashedcontrollers.sportsontv;
-
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-
 public class Splash extends AppCompatActivity {
     ImageView imageView;
+    MediaPlayer mediaPlayer1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mediaPlayer1 = MediaPlayer.create(this,R.raw.cheer);
+        mediaPlayer1.start();
+
+
+        //make sure screen stays at current orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         imageView = (ImageView)findViewById(R.id.logoIV);
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_animation);
         imageView.setAnimation(animation);
@@ -20,18 +27,14 @@ public class Splash extends AppCompatActivity {
         animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-
             }
-
             @Override
             public void onAnimationEnd(Animation animation) {//on animation end start new class
                 finish();
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
             }
-
             @Override
             public void onAnimationRepeat(Animation animation) {
-
             }
         });
     }

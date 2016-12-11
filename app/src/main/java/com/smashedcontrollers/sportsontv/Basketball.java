3d524695b92f7837618b967c398a06d05301909f
Luke Smy <smywalker22@gmail.com>
2016-12-11 23:12:55 +0000

    Implemented Firebase list

--- app/src/main/java/com/smashedcontrollers/sportsontv/Basketball.java
+++ app/src/main/java/com/smashedcontrollers/sportsontv/Basketball.java
@@ -2,21 +2,65 @@ package com.smashedcontrollers.sportsontv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Basketball extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class Basketball extends AppCompatActivity {
+    DatabaseReference dref;
+    ListView newListView;
+    ArrayList<String> list = new ArrayList<>();
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basketball);
+        newListView=(ListView)findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.
+        simple_dropdown_item_1line, list);
+        newListView.setAdapter(adapter);
+        dref=FirebaseDatabase.getInstance().getReference();
+        dref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                list.add(value);
+                adapter.notifyDataSetChanged();
+            }
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-        myRef.setValue(" dave beckham");
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                list.remove(value);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        //FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //DatabaseReference myRef = database.getReference("message");
+
+        //myRef.setValue(" dave beckham");
     }
 
 
